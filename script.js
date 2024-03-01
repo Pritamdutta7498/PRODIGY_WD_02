8cf6ff960d50e216b89f0c5eb8b8a9236f1fba69
PritamDutta <webdevpritam7498@gmail.com>
2024-03-01 20:31:27 +0600

    js function and statement added for showing and running the stopwatch

--- script.js
+++ script.js
@@ -6,5 +6,30 @@ const timeElement = document.getElementById("time");
 const lapsElement = document.getElementById("laps");
 
 // console.log("startButton", startButton);
-// console.log(lapButton);
+// console.log(lapsElement);
+
+let hours = 0;
+let minutes = 0;
+let seconds = 0;
+let milliseconds = 0;
+let intervalId;
+let lapTimes = [];
+
+// timing setup function added
+function updateTime() {
+    milliseconds++;
+    if (milliseconds === 100) {
+        milliseconds = 0;
+        seconds++;
+        if (seconds === 60) {
+            seconds = 0;
+            minutes++;
+            if (minutes === 60) {
+                minutes = 0;
+                hours++;
+            }
+        }
+    }
+    
+}
 
