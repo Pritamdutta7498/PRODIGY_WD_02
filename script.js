22e035144ed2f6da3cb3e2365223cfc9272eea35
PritamDutta <webdevpritam7498@gmail.com>
2024-03-01 12:35:55 +0600

    find all btn and element  with id for using the function

---
+++ script.js
@@ -0,0 +1,10 @@
+const startButton = document.getElementById("start");
+const pauseButton = document.getElementById("pause");
+const resetButton = document.getElementById("reset");
+const lapButton =   document.getElementById("lap");
+const timeElement = document.getElementById("time");
+const lapsElement = document.getElementById("laps");
+
+// console.log("startButton", startButton);
+// console.log(lapButton);
+
